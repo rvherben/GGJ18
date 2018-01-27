f3058f3cb9ce2bbd0c6b6f94aec4fa15eaca056c
Julian Zipper <dejuliman@live.nl>
2018-01-27 12:24:05 +0100

    add different script

---
+++ GGJ18Game/Assets/Scripts/RotationController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotationController : MonoBehaviour {
+
+	private float baseAngle = 0f;
+
+	void OnMouseDown()
+	{
+		Debug.Log ("YO WTF MOUSEDOWN?");
+		var dir = Camera.main.WorldToScreenPoint(transform.position);
+		dir = Input.mousePosition - dir;
+		baseAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		baseAngle -= Mathf.Atan2(transform.right.y, transform.right.x) * Mathf.Rad2Deg;
+	}
+
+	void OnMouseDrag()
+	{
+		Debug.Log ("YO WTF MOUSE DRAAAAAAG");
+		var dir = Camera.main.WorldToScreenPoint(transform.position);
+		dir = Input.mousePosition - dir;
+		var angle =  Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - baseAngle;
+		transform.eulerAngles = Quaternion.AngleAxis(angle, Vector3.forward).eulerAngles;
+	}
+}
